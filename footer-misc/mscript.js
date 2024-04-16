9741ddb25ac4fcd133f09f6be5b9e3d76a9ac183
CreeperMain <creepermaingaming@gmail.com>
2024-04-16 22:40:41 +0200

    display content

--- footer-misc/mscript.js
+++ footer-misc/mscript.js
@@ -4,6 +4,23 @@ document.addEventListener("DOMContentLoaded", () => {
   const btn3 = document.getElementById("btn3");
   const stvari = document.getElementById("stvari2");
 
+  async function fetchHTMLstvari() {
+    try {
+      const response = await fetch("../footer-misc/pages/contactpage.html");
+
+      if (!response.ok) {
+        throw new Error("ne rab");
+      }
+
+      const htmlContent = await response.text();
+
+      stvari.innerHTML = htmlContent;
+    } catch (error) {
+      console.error("rabotit?", error);
+    }
+  }
+  fetchHTMLstvari();
+
   btn1.addEventListener("click", () => {
     fetch("../footer-misc/pages/contactpage.html")
       .then((response) => response.text())
